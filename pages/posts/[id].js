2f971185c1a190d460bdba265c84bffd8f62ae7c
仲地晴紀 <nakatievent@gmail.com>
2021-08-12 19:55:31 +0900

    work-branch　個別投稿ページの実装

---
+++ pages/posts/[id].js
@@ -0,0 +1,52 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import Layout from "../../components/Layout"
+import { getAllPostsIds, getPostData } from "../../lib/posts"
+
+export default function Post({ post }) {
+    const router = useRouter()
+
+    if (!post) {
+        return (
+            <div>Loading</div>
+        )
+    }
+
+    return (
+        <Layout title={post.title}>
+            <p>
+                {"ID : "}
+                {post.id}
+            </p>
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+            <Link href="/blog-page">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+                    </svg>
+                    <span>前のページへ戻る</span>
+                </div>
+            </Link>
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = await getAllPostsIds()
+
+    return {
+        paths,
+        fallback: true //pathsに定義されていないアクセスがあった場合、404ページを返却する
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const { post: post } = await getPostData(params.id)
+
+    return {
+        props: {
+            post
+        }
+    }
+}
