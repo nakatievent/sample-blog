ba4118318d9da543ad754f7d904c9099c1bf2074
仲地晴紀 <nakatievent@gmail.com>
2021-08-22 01:35:17 +0900

    work-branch　軽微な修正

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Footer from './Footer'
 import Header from './Header'
 
-export default function Layout({ children, title = "Default Title"}) {
+export default function Layout({ children, title = "Default Title" }) {
     return (
         <div>
             <Head>
